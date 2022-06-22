967e430c9bf4c64a535bd14835430c971de6440b
Falodun Damilola <dpaulzs11@gmail.com>
2022-06-23 00:14:45 +0100

    adjusted menu

--- src/Components/Navigation/Menu.jsx
+++ src/Components/Navigation/Menu.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
 import { useGlobalContext } from '../../Content-Manager/Context';
+import energy from '../../Assets/Navigation-Images/energy.png';
+import usericon from '../../Assets/Navigation-Images/usericon.png';
+import arrowdown from '../../Assets/Navigation-Images/arrowdown.png';
+import logout from '../../Assets/Navigation-Images/logout.png';
 
-const Menu = ({setMenu, log}) => {
-    const {  } = useGlobalContext()
+const Menu = ({setMenu, log, setLog}) => {
+    const { user, totalEnergy } = useGlobalContext()
 
     return(
         <aside className='menu'>
@@ -12,7 +16,10 @@ const Menu = ({setMenu, log}) => {
                 <Link to='/future' className='menu-link'onClick={() => setMenu(false)}><li>Future editions</li></Link>
                 <Link to='/about' className='menu-link' onClick={() => setMenu(false)}><li>About us</li></Link>
                 <Link to='/donate' className='menu-link' onClick={() => setMenu(false)}><li>Make a donation</li></Link>
-                {!log && <>
+                {user ? <div className={`${ log && 'nav-out'} nav-user`}>
+                        <p> <img src={energy} alt="energy" /> {totalEnergy} </p>
+                        <p onClick={() => setLog(!log)}> <img src={usericon} alt="user" /> {user} <img src={arrowdown} alt="arrow"  /></p> </div>
+                        : <>
                 <Link to='/sign' className='menu-link'><li onClick={() => setMenu(false)}>Log In</li></Link>
                 <Link to='/create' className='menu-link'><li onClick={() => setMenu(false)}>Create Account</li></Link> </>}
             </ul>
